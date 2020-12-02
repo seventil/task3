9dbdb867f3d2fd769e5cc96880c5d4622daa24ce
Ales Kutsepau <ales.kutepov@gmail.com>
2020-12-02 17:23:37 +0300

    Update task_3.py
    added high-level function scheduler() that handles input of a function to execute in a queue.

--- task_3.py
+++ task_3.py
@@ -11,17 +11,23 @@ class MockOperation():
     def function(self):
         next(self.a)
 
-
     def value(self):
         return self.a
+
+
+def scheduler(queue: queue.Queue, function_to_schedule, arg=None):
+    if arg is None:
+        queue.put(function_to_schedule)
+    else:
+        for _ in range(arg):
+            queue.put(function_to_schedule)
 
 
 def main():
     mock_object = MockOperation()
     q = queue.Queue()
 
-    for _ in range(6):
-        q.put(mock_object.function)
+    scheduler(q, mock_object.function, 5)
 
     with concurrent.futures.ThreadPoolExecutor() as executor:
         for _ in range(5):
