703c685e1bbff57580aac5019aff1b858191dcd2
Ales Kutsepau <ales.kutepov@gmail.com>
2020-12-02 21:05:38 +0300

    Update task_3.py
    added worker fucntion, that takes a scheduled task from the queue and executes it, also added daemon threads that run workers until the end of the queue

--- task_3.py
+++ task_3.py
@@ -1,37 +1,51 @@
 import concurrent.futures
-import itertools
 import queue
+from threading import Thread
 
 
-class MockOperation():
+class MockOperation:
 
     def __init__(self):
-        self.a = itertools.count()
+        self.a = 0
 
     def function(self):
-        next(self.a)
+        self.a += 1
 
     def value(self):
         return self.a
 
 
-def scheduler(queue: queue.Queue, function_to_schedule, arg=None):
+def scheduler(q: queue.Queue, function_to_schedule, arg=None):
     if arg is None:
-        queue.put(function_to_schedule)
+        q.put(function_to_schedule)
     else:
         for _ in range(arg):
-            queue.put(function_to_schedule)
+            q.put(function_to_schedule)
+
+
+def worker(q: queue.Queue):
+    while True:
+        a = q.get()
+        a()
+        q.task_done()
 
 
 def main():
     mock_object = MockOperation()
     q = queue.Queue()
 
-    scheduler(q, mock_object.function, 5)
+    # потоки, которые исполняют воркеров
+    for _ in range(5):
+        t = Thread(target=worker, args=(q,))
+        t.setDaemon(True)
+        t.start()
 
-    with concurrent.futures.ThreadPoolExecutor() as executor:
+    # потоки, которые добавляют функции, запланированные к исполнению в очередь
+    with concurrent.futures.ThreadPoolExecutor() as master:
         for _ in range(5):
-            executor.submit(q.get())
+            master.submit(scheduler(q, mock_object.function, 100000))
+
+    q.join()
 
     print("----------------------", mock_object.value())
 
